9e9ca515248c63785127d25d273de9215db1c004
kev1nandreas <kevin.andreascn@gmail.com>
2025-02-22 22:05:49 +0700

    feat: update Category page and add new dependencies for state management

--- src/app/admin/kategori/page.tsx
+++ src/app/admin/kategori/page.tsx
@@ -1,11 +1,7 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
 import Sidebar from '@/components/dashboard/Sidebar';
 import CategorySection from '@/components/dashboard/CategorySection';
-import { useRouter } from 'next/navigation';
-import Authenticating from '@/app/authenticating';
-import { Bounce, toast } from 'react-toastify';
 
 const Category = () => {
   return (
